3b2e32e5941ed606917336f20c7886ce95873085
Alioune Seck <aliouneseck15@gmail.com>
2023-12-02 18:22:37 -0500

    update board model

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
 /*----- constants -----*/
-
+const IDENTIFIERS = {
+    '0': 'white',
+    'X': 'X',
+    'O': 'O'
+};
 
 /*----- state variables -----*/
-
+let turn;
+let board;
+let winner;
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('#message');
+const cells = document.querySelectorAll('.cell');
 
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleClick);
 
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
+init();
+
+function init() {
+    turn = 1;
+    board = [
+        [0, 0, 0], // col 0
+        [0, 0, 0], // col 1
+        [0, 0, 0]  // col 2
+    ];
+    winner = null;
+    render();
+}
+
+function handleClick(evt) {
+    const cell = evt.target;
+    const col = cell.parentNode.cellIndex;
+    const row = cell.parentNode.parentNode.rowIndex - 1;
+    if (board[row][col] || winner) {
+        return
+    };
+    board[row][col] = turn;
+    turn *= -1;
+    checkForWin();
+    render();
+}
